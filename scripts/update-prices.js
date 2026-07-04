d48376b2554bfa7bb9315a9385d2b5882d1fdedf
Phoeurn Sokpheng <channyhong520@gmail.com>
2026-07-04 15:33:14 +0700

    Initial upload of web store

--- scripts/update-prices.js
+++ scripts/update-prices.js
@@ -6,8 +6,8 @@ const { Client } = require('pg');
   const client = new Client({ connectionString: process.env.DATABASE_URL });
   await client.connect();
 
-  await client.query("UPDATE source_codes SET price = '29.99' WHERE id = 1");
-  await client.query("UPDATE source_codes SET price = '4.99' WHERE id = 2");
+  await client.query("UPDATE source_codes SET price = '0.01' WHERE id = 1");
+  await client.query("UPDATE source_codes SET price = '29.99' WHERE id = 2");
 
   const res = await client.query("SELECT id, title, price FROM source_codes ORDER BY id");
   console.log(JSON.stringify(res.rows, null, 2));
